6e18e4d40a3acbee2b2fbc8b8ff44a374ddea6bc
Xiaochen Lu <xiaoclu@ebay.com>
2023-05-25 16:35:32 +0800

    swap nodes in pairs 3rd repeat

---
+++ LinkedList/24_SwapNodesInPairs.py
@@ -0,0 +1,54 @@
+"""
+Given a linked list, swap every two adjacent nodes and return its head.
+You must solve the problem without modifying the values in the list's nodes 
+(i.e., only nodes themselves may be changed.)
+
+
+Example 1:
+Input: head = [1,2,3,4]
+Output: [2,1,4,3]
+
+
+Example 2:
+Input: head = []
+Output: []
+
+
+Example 3:
+Input: head = [1]
+Output: [1]
+ 
+
+Constraints:
+The number of nodes in the list is in the range [0, 100].
+0 <= Node.val <= 100
+"""
+
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+
+        sentinel = ListNode(val=None, next=head)
+        zeroth, first, second = sentinel, head, head.next
+
+        while first and second:
+            third = second.next
+
+            zeroth.next = second
+            second.next = first
+            first.next = third
+
+            zeroth, first, second = first, third, third.next if third else None
+
+        return sentinel.next
