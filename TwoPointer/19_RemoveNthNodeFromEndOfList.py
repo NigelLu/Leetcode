2530c6a1704a82fa52975ccea99a9b9feafba2b3
Xiaochen Lu <xiaoclu@ebay.com>
2023-07-21 15:03:56 +0800

    add: remove nth node from end of list 4th repeat

---
+++ TwoPointer/19_RemoveNthNodeFromEndOfList.py
@@ -0,0 +1,54 @@
+"""
+Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+
+Example 1:
+Input: head = [1,2,3,4,5], n = 2
+Output: [1,2,3,5]
+
+Example 2:
+Input: head = [1], n = 1
+Output: []
+
+Example 3:
+Input: head = [1,2], n = 1
+Output: [1]
+ 
+Constraints:
+The number of nodes in the list is sz.
+1 <= sz <= 30
+0 <= Node.val <= 100
+1 <= n <= sz
+ 
+Follow up: Could you do this in one pass?
+"""
+
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        if not head:
+            return
+        sentinel = ListNode(None, head)
+
+        prev = sentinel
+        left = right = head
+        for _ in range(n - 1):
+            right = right.next
+            if not right:
+                return
+
+        while right.next:
+            prev, left, right = prev.next, left.next, right.next
+
+        prev.next = left.next
+
+        return sentinel.next
